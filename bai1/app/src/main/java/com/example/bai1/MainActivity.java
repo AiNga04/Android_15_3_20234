3b8b3411499c729ca5a3cd6f98fc3e11a78f842a
AiNga04 <aingaspkt@gmail.com>
2024-03-15 16:33:25 +0700

    22162027

---
+++ bai1/app/src/main/java/com/example/bai1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.bai1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.bai1.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private UserModel userModel;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        userModel = new UserModel("Ai", "Nga");
+        binding.setUser(userModel);
+
+        // them tu cap nhat
+        userModel.setFirstName("Truong");
+        userModel.setLastName("Nga");
+    }
+}
